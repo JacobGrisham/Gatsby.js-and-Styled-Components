bf50fc8b23e2cc5b5204bd7d32c5055493668642
Jacob Grisham <jacob.d.grisham@gmail.com>
2021-07-11 17:41:22 -0700

    add Gatsby plugins and define gatsby-source-filesystem

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,20 +3,39 @@ module.exports = {
     title: "Star Wars Models and Blueprints",
   },
   plugins: [
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
     "gatsby-plugin-styled-components",
+    `gatsby-plugin-netlify`,
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+    `gatsby-plugin-portal`,
     {
       resolve: "gatsby-plugin-google-analytics",
       options: {
         trackingId: "G-36KQSD0BWH",
       },
     },
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sitemap",
     {
       resolve: "gatsby-plugin-manifest",
       options: {
-        icon: "src/images/icon.png",
+        icon: "src/assets/icon.png",
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/pages/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `data`,
+        path: `${__dirname}/src/assets/`,
+      },
+    }
   ],
 };
