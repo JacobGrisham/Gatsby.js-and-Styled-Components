f2291f71acc2585b98e374c92c0891835a1ad023
Jacob Grisham <jacob.d.grisham@gmail.com>
2021-07-11 18:35:30 -0700

    add PWA support and robots.txt Gatsby plugins. update gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,16 +1,20 @@
 module.exports = {
   siteMetadata: {
-    title: "Star Wars Models and Blueprints",
+    title: "Star Wars 3D Models and Blueprints",
+    siteUrl: 'https://www.example.com',
+    description: 'View iconic Star Wars space ships like the Millennium Falcon and a Tie Fighter as an interactive 3D model and as a blueprint.',
+    author: 'Jacob Grisham'
   },
   plugins: [
+    `gatsby-plugin-portal`,
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
     "gatsby-plugin-styled-components",
-    `gatsby-plugin-netlify`,
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-sitemap",
-    `gatsby-plugin-portal`,
+    'gatsby-plugin-robots-txt',
+    `gatsby-plugin-netlify`,
     {
       resolve: "gatsby-plugin-google-analytics",
       options: {
@@ -36,6 +40,21 @@ module.exports = {
         name: `data`,
         path: `${__dirname}/src/assets/`,
       },
-    }
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Star Wars 3D Models and Blueprints`,
+        short_name: `Star Wars`,
+        start_url: `/`,
+        background_color: `#000000`,
+        theme_color: `#FFF`,
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: `standalone`,
+        icon: `src/assets/icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    `gatsby-plugin-offline`,
   ],
 };
